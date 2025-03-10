debe450aa9ef2a5b3dbc9320e771e328cb17f45b
Marsol4 <enzogmarsola@gmail.com>
2025-03-10 10:37:15 -0300

    Cria a Classe PhotoMapping e adiciona a sua tipagem inicial

---
+++ TDSKP.API/Infrastructure/Mappings/PhotoMapping.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TDSKP.API.Infrastructure.Persistence;
+
+namespace TDSKP.API.Infrastructure.Mappings
+{
+    public class PhotoMapping : IEntityTypeConfiguration<Photo>
+    {
+        public void Configure(EntityTypeBuilder<Photo> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
