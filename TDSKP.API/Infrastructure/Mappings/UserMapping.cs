fe221635541f8ca52bf0e61fe90e9f4aa33493f9
Marsol4 <enzogmarsola@gmail.com>
2025-03-10 10:56:09 -0300

    feat: mapping User

---
+++ TDSKP.API/Infrastructure/Mappings/UserMapping.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TDSKP.API.Infrastructure.Persistence;
+
+namespace TDSKP.API.Infrastructure.Mappings
+{
+    public class UserMapping : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey("Id");
+
+            builder.Property(user => user.Name)
+                .IsRequired();
+        }
+    }
+}
